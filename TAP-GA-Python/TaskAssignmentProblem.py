1a50c6a8fb41d178e0941aeccb44d2e66e9ba771
talhavawda <talhavawda.dev@gmail.com>
2020-12-09 22:41:27 +0200

    Comments for main() and its caller functions

--- TAP-GA-Python/TaskAssignmentProblem.py
+++ TAP-GA-Python/TaskAssignmentProblem.py
@@ -12,8 +12,8 @@
 					PyCharm 2019.3.3 (Professional Edition) Build #PY-193.6494.30
 
 				Acknowledgements:
-					search.py
-					utils.py
+					search.py 	(https://github.com/aimacode/aima-python/blob/master/search.py)
+					utils.py	(https://github.com/aimacode/aima-python/blob/master/utils.py)
 
 				Notes:
 					1.	Due to the assignment's requirement of this single script being able to be run from the command line,
@@ -30,10 +30,11 @@
 import random
 import bisect
 
-
 """
 	Helper Functions (from utils.py)
 """
+
+
 def weighted_sampler(seq, weights):
 	"""Return a random-sample function that picks from seq weighted by weights."""
 	totals = []
@@ -42,11 +43,9 @@ def weighted_sampler(seq, weights):
 	return lambda: seq[bisect.bisect(totals, random.uniform(0, totals[-1]))]
 
 
-
 """
 	Genetic Search Algorithm (taken from search.py)
 """
-
 
 
 def genetic_search(problem, ngen=1000, pmut=0.1, n=20):
@@ -62,7 +61,7 @@ def genetic_search(problem, ngen=1000, pmut=0.1, n=20):
 	random.shuffle(states)
 	return genetic_algorithm(states[:n], problem.value, ngen, pmut)
 
-
+# ngen - number of generations of the population
 def genetic_algorithm(population, fitness_fn, gene_pool=[0, 1], f_thres=None, ngen=1000, pmut=0.1):
 	"""[Figure 4.8]"""
 	for i in range(ngen):
@@ -103,6 +102,13 @@ def init_population(pop_number, gene_pool, state_length):
 
 def select(r, population, fitness_fn):
 	fitnesses = map(fitness_fn, population)
+	"""
+		map() function returns a map object(which is an iterator) of the results after applying the given function to each item of a given iterable (list, tuple etc.)
+		Params:
+			fun : It is a function to which map passes each element of given iterable.
+			iter : It is a iterable which is to be mapped.
+	"""
+
 	sampler = weighted_sampler(population, fitnesses)
 	return [sampler() for i in range(r)]
 
@@ -142,39 +148,62 @@ def mutate(x, gene_pool, pmut):
 # ==============================================================================
 
 
-#Todo
-def solveTaskAssignment(table: list, persons:int = None, tasks: int = None):
-
-	if not persons: # i.e. if persons == None (the default) - if no persons parameter was passed in
+# Todo
+def solveTaskAssignment(table: list, persons: int = None, tasks: int = None):
+	if not persons:  # i.e. if persons == None (the default) - if no persons parameter was passed in
 		persons = len(table)
 
-	if not tasks:
+	if not tasks:  # i.e. if tasks == None (the default) - if no tasks parameter was passed in
 		tasks = len(table[0])
 
 	optimalAssignment = []
 
-	totalScoreOptimal = fitness_funct(optimalAssignment)
+	#totalScoreOptimal = fitness_funct(optimalAssignment)
 
-	return  optimalAssignment, totalScoreOptimal
+	#return optimalAssignment, totalScoreOptimal
 
 
 # ==============================================================================
 
 
 def getPersonScores(tasks: int, scoresStr: str):
+	"""
+		Get the list of scores (Performance Values) for a Person from a string (a line from the textfile) containing them
+
+		:param tasks: the number of Tasks in the problem (the Person will have a score for each)
+		:param scoresStr: the scores of the Person represented as a string
+		:return: the scores of the Person for the Tasks represented as a list
+	"""
+
 	scores: int = []  # 1D list
+
 	for i in range(tasks):  # i traverses [0, ..., tasks-1]
+
 		scoresStr = scoresStr.strip()  # Remove leading and trailing whitespaces so that the beginning of the string is the score for this task
+
+		"""
+			The index of the first whitespace in the string
+			Score values are separated by at least one whitespace, so this variable is the index after the end of the 
+			value of the current task in the string, and we can use it to extract the value of the current task from the string
+			(The value of the current task will be the first value in the string as we delete/remove the values 
+			from the string once we finish extract them)
+		"""
 		whitespaceIndex = scoresStr.find(" ")
 
 		"""
 			If we are at the last task (i == tasks-1), then  there wont be a whitespace after the 
-			score for this task in the scoresStr, so dont do any slicing - the score (as a string) for this task
-			is the remaining scoresStr
+			score for this task in the scoresStr (whitespaceIndex will be -1), so dont do any slicing - the score (as a string)
+			for this task is the remaining scoresStr
 
 			Else (if not the last task, thus there is a whitespace between the score for this task and the next task)
 			then to get the score (as a string) for this task, slice from the beginning of the scoresStr till the character
 			before the whitespace
+			
+			If the actual number of tasks in the textfile is more than the tasks variable (this occurs when num of tasks
+			is greater than num of persons, so we reduce the number of tasks we using), this algorithm still works - since
+			there'll be a whitespace (followed by a value) in the string, the else part wll execute, extracting just the value
+			for this (last) task (last task based on the number of tasks we using) instead of the remainder of the string,
+			and the loop will terminate (thus we don't also extract the values of the extra tasks from the textfile) 
 		"""
 		if whitespaceIndex == -1:
 			scoreStr = scoresStr
@@ -189,71 +218,127 @@ def getPersonScores(tasks: int, scoresStr: str):
 
 
 def readInputFile(fileName: str):
+	"""
+		Reads in the input textfile containing the Person-Task performance values table
+
+		The Assignment Specification Document specifies that the first line of the textfile is
+		the number of persons (rows in the table),	the second line is the number of tasks (columns in the table),
+		and the remainder lines is the table/matrix (without the row and column headings; just the raw performance values)
+
+		The lecturer mentioned in the class meeting that we must assume that the number of persons is the same as the number of tasks
+
+		:param fileName: the name of the input textfile
+		:return: the Person-Task performance-values table, number of persons, number of tasks (as a tuple in that order)
+	"""
+
 	inputFile = open(fileName)
 
 	persons = int(inputFile.readline())  # number of persons
 	tasks = int(inputFile.readline())  # number of tasks
 
-	# table = inputFile.readlines()
-	table = []  # This is going to be a 2D Array/list
+	"""
+		Specification: assume that the number of persons is the same as the number of tasks
+		Thus if num persons != num tasks, make the number of persons be the number of tasks 
+		(by reducing the one that is bigger to the one that is smaller such that we get a 'square' table; we
+		can't increase the smaller parameter to the bigger one as we don't have any performance values that we can add)
+	"""
+	if persons > tasks:
+		persons = tasks
+	elif tasks > persons:
+		tasks = persons
 
+	"""
+		This table variable is a 2D list/array which represents the Person-Task performance-values table
+		It is a list of lists with each sublist being a row in the table - each sublist represents a person.
+		For sublist i, the value at index j is how well Person i performs Task j (i.e. the performance value)
+	"""
+	table = []
+
+	"""
+		For each person, get their row (their list of scores/values for the Tasks) from the Person-Task table in the textfile
+		and add it to the table 2D list
+		We skip over any empty/blank lines we come across (this is to cater for blank lines between the number of tasks line
+		and the start of the actual table in the textfile)
+	"""
 	for person in range(persons):
+
 		while True:  # Get the next non-empty/blank line (to skip over any blank lines between the number of tasks and the table itself)
 			personScoresString = inputFile.readline()
 			if personScoresString != "\n":
 				break
-		# print(personScoresString)
+
 		personScores = getPersonScores(tasks, personScoresString)
 		table.append(personScores)
-	# print(personScores)
+
 
 	inputFile.close()
 
-	return  table, persons, tasks
+
+	return table, persons, tasks
 
 
 
+def printPTTable(table: list, persons: int = None, tasks: int = None):
+	"""
+		Display (to the terminal) the Person-Task performance-values table
 
-def printPTTable(table: list, persons:int = None, tasks: int = None):
+		:param table: 	the 2D list/array of performance values (scores)
+		:param persons: number of Persons
+		:param tasks: 	number of Tasks
+		:return: None
+	"""
 
-	if not persons: # i.e. if persons == None (the default) - if no persons parameter was passed in
+	if not persons:  # i.e. if persons == None (the default) - if no persons parameter was passed in
 		persons = len(table)
 
-	if not tasks:
+	if not tasks:	# i.e. if tasks == None (the default) - if no tasks parameter was passed in
 		tasks = len(table[0])
 
-
-	#Column Headings (Tasks)
+	# Column Headings (Tasks)
 	print("Tasks ->", end="\t\t")
 	headerStr = "=============="
+
+	#Tasks are labelled/identified using numbers/digits so first task is Task 1 (index 0), 2nd is Task 2, ..., and so on
 	for task in range(tasks):
-		print(task+1, end="\t")
+		print(task + 1, end="\t")
 		headerStr += "===="
+
 	print("\nPersons:")
 	print(headerStr)
 
 
-	#for person in table: # for each row
-	#	print(end="\t\t\t\t")
-	#	for task in person: # for each column in a row
-	#		print(task, end="\t")
-
-	for person in range(persons): # for each row
-		print("\t", person+1, end="\t|\t\t")
-		for task in table[person]: # for each column in a row
-			print(task, end="\t")
+	"""
+		Persons are labelled/identified using Uppercase letters of the alphabet so first person is Person A (index 0; ASCII-65),
+		2nd is Person B (index 1; ASCII-66), ... and so on
+		
+		So Person at index i has ASCII value of 65+i
+	"""
+	for person in range(persons):  # for each row
+		print("\t", chr(person+65), end="\t|\t\t")
+		for task in table[person]:  # for each column in a row
+			print(task, end="\t") # Display the performance value of this person doing this task
 		print()
 
 
 
-
 def main():
+	"""
+		The main function of the program
+		It gets the input textfile name from the user, reads in the textfile to get the Person-Task performance values table,
+		solves the Task Assignment Problem, and displays the results (by calling the relevant functions)
+
+	:return: None
+
+	"""
 	print("\n\nSolving the Task Assignment Problem using Genetic Algorithms")
 	print("=============================================================\n\n")
 
 	fileName = "input.txt"  # Set the default filename to 'input.txt'
-	userFile = input("Please enter the file name of the Person-Task performance values: ")
-	print("If no absolute path to the file was specified, the file is assumed to be in the current working directory")
+	print("Please enter the file name of the Person-Task performance values")
+	print(
+		"Note:\tIf no absolute path to the file is specified, the file is assumed to be in the current working directory")
+	print("\t\tIf the Enter key is pressed without entering any filename, 'input.txt' will be used as the file name")
+	userFile = input("File Name: ")
 
 	userFile = userFile.strip();
 
@@ -262,8 +347,7 @@ def main():
 
 	table, persons, tasks = readInputFile(fileName)
 
-	optimalAssignment, totalScoreOptimal = solveTaskAssignment(table, persons, tasks)
-
+	#optimalAssignment, totalScoreOptimal = solveTaskAssignment(table, persons, tasks)
 
 	print()
 	print("Persons:\t", persons)
@@ -278,4 +362,3 @@ def main():
 
 
 main()
-
